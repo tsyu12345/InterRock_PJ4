7b11ba28ef758916d7bdfa7f88c71962b8df7c39
syu_asus_Note <syukunt@icloud.com>
2021-10-25 09:47:07 +0900

    https://neos21.net/blog/2020/01/31-03.htmlにてspyderの試験実装。

--- SecondTest/SecondTest/spiders/SecondSpyder.py
+++ SecondTest/SecondTest/spiders/SecondSpyder.py
@@ -7,4 +7,13 @@ class SecondspyderSpider(scrapy.Spider):
     start_urls = ['http://blog.scrapinghub.com/']
 
     def parse(self, response):
-        pass
+        for older_link in response.css('.post-listing .post-item'):
+            print(older_link)
+            yield Post(
+                url=post.css('div.post-header a::attr(href)').extract_first().strip(),
+            )
+        older_link = response.css('.blog-pagination a.next-posts-link::attr(href)').extract_first()
+        if older_link is None:
+            return 
+        older_link = response.urljoin(older_link)
+        yield scrapy.Request(older_link, callback=self.parse)
