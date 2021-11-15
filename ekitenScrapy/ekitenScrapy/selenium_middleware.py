80e64bb4981fea56a94cc4daa8762b553397791c
syu_asus_Note <syukunt@icloud.com>
2021-11-15 09:51:17 +0900

    seleniumのミドルウェアをかますことで、Jsで動的に生成されるページをロードできることはわかった。

--- ekitenScrapy/ekitenScrapy/selenium_middleware.py
+++ ekitenScrapy/ekitenScrapy/selenium_middleware.py
@@ -1,19 +1,29 @@
-import os.path
-
-from urlparse import urlparse
-
-import arrow
-
+from scrapy.http import HtmlResponse
 from selenium import webdriver
-from scrapy.http import HtmlRsesponse
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Chrome()
-
-class SeleniumMiddleware(object):
-
-    def process_request(self, request, spider):
-        driver.get(request.url)
-        return HtmlRsesponse(driver.current_url, body=driver.page_source, encoding='utf-8', request=request)
+class SelenDriver():
     
-    def quitDriver():
-        driver.quit()
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path='chromedriver.exe')
+        
+    def get_dom(self, query):
+        dom = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, query)))
+        return dom
+
+    def driver_quit(self):
+        self.driver.delete_all_cookies()
+        self.driver.quit()
+    
+    def get_page(self, url):
+        self.driver.get(url)
+class ScrapyMiddleware(object):
+    
+    def process_request(self, request, spider):
+        driver = SelenDriver()
+        driver.driver.get(request.url)
+        return HtmlResponse(driver.driver.current_url, body=driver.driver.page_source, encoding='utf-8', request=request)
+    
+
--- ekitenScrapy/ekitenScrapy/spiders/ekitenSpider.py
+++ ekitenScrapy/ekitenScrapy/spiders/ekitenSpider.py
@@ -1,21 +1,25 @@
+import selenium
 import scrapy
 from ekitenScrapy.items import EkitenscrapyItem, StoreUrlItem
-from ..selenium_middleware import quitDriver
+from ..middlewares import *
+from ..selenium_middleware import *
 class EkitenspiderSpider(scrapy.Spider):
     name = 'ekitenSpider'
     allowed_domains = ['ekiten.jp']
     start_urls = ['https://www.ekiten.jp/area/a_prefecture36/'] #一旦試験的に、徳島のみ。
+    
     custom_settings = {
         "DOWNLOADER_MIDDLEWARES": {
-            "some_crawler.selenium_middleware.SeleniumMiddleware": 0,
+            "ekitenScraapy.selenium_middleware.ScrapyMiddleware",
         },
         "DOWNLOAD_DELAY": 0.5,
     }
+    
     def parse(self, response):
         item = EkitenscrapyItem() #本スクレイピング用
         preItem = StoreUrlItem() #遷移先URL格納用
 
-        for url in response.css('body > div.l-wrapper > div > div.l-contents_wrapper > div > nav > div:nth-child(1) > ul > li:nth-child(2) > div > div > div > div > div > ul > li > div.grouped_list_body > ul > li > a'):
+        for url in response.css('body > div.l-wrapper > div > div.l-contents_wrapper > div > nav > div:nth-child(1) > ul > li:nth-child(2) > div > div > div'):
             #市区町村の絞り込み
             manufacUrl = url.css('a::attr(href)').extract_first()
             print(manufacUrl)
@@ -24,4 +28,4 @@ class EkitenspiderSpider(scrapy.Spider):
             yield item
 
     def closed(self, reason):
-        quitDriver()
+        SelenDriver.driver_quit()
