3d0a88f0307e7110ff23165c46e1eb7ec00410a4
syu_asus_Note <syukunt@icloud.com>
2021-12-03 10:43:27 +0900

    次ページがある場合にはへ遷移するような操作を追加。しかしIndexErrorが発生している。400エラーに関しては一応対策はしたが未検証。だがやはりレスポンスの間隔が問題ポイ挙動を繰り返していた。

--- ekitenScrapy/ekitenScrapy/spiders/ekitenSpider.py
+++ ekitenScrapy/ekitenScrapy/spiders/ekitenSpider.py
@@ -12,24 +12,25 @@ from ..selenium_middleware import SeleniumMiddlewares
 class EkitenspiderSpider(scrapy.Spider):
     name = 'ekitenSpider'
     allowed_domains = ['ekiten.jp']
-    start_urls = ['https://www.ekiten.jp/cat_golf/tokushima/ananshi/']
+    start_urls = ['https://www.ekiten.jp/area/a_prefecture36/']
     MAX_RRTRYCOUNT = 3
     RETEYED = 0
-    
+    """
     def start_requests(self):
-        """Summary Lines
+        Summary Lines
         店舗URLを取得する前処理。各ジャンルのページリンクを取得する。
         Yields:
             str: middlewareで返却された小ジャンルURL
-        """
+        
         middleware = SeleniumMiddlewares(['徳島県'], 4)
         result = middleware.run()
         for url in result:
             yield scrapy.Request(url, callback=self.pre_parse, errback=self.error_parse)
-        
+    """ 
     def error_parse(self, failure):
         """Summary Lines
-        scrapy.Requestで例外発生時（response.stasusが400、500台）にcallbackする。
+        scrapy.Requestで例外発生時（response.stasusが400、500台）にcallbackする。\n
+        一定時間後にリトライリクエストする。\n
         Args:
             failure (scrapy.Request): scrapy.Request
         """
@@ -38,10 +39,14 @@ class EkitenspiderSpider(scrapy.Spider):
         while self.RETEYED < self.MAX_RRTRYCOUNT:
             time.sleep(20)
             response = failure.value.response
-            yield scrapy.Request(response.url, callback=self.pre_parse, errback=self.error_parse)
+            yield scrapy.Request(
+                response.url, 
+                callback=self.pre_parse, 
+                errback=self.error_parse, 
+                dont_filter=True)
             self.RETEYED += 1 
           
-    def pre_parse(self, response):
+    def parse(self, response):
         """Summary Lines
         店舗検索処理。スクレイピング処理をする店舗URLを取得する。
         Args:
@@ -52,16 +57,32 @@ class EkitenspiderSpider(scrapy.Spider):
         """
         #self.start_urls = self.search(response)
         print(type(response.status))
-        self.RETEYED = 0
+        self.RETEYED = 0 #成功したらリトライカウントをリセット
         item = EkitenscrapyItem()
+        parse_urls = []
         for elm in response.css('div.layout_media.p-shop_box_head > div.layout_media_wide > div > h2 > a'):
             href = elm.css('a::attr(href)').extract_first()
             url = response.urljoin(href)
-            yield scrapy.Request(url)
-    
+            yield scrapy.Request(url, callback=self.parse, errback=self.error_parse)
+        
+        #次のページがあるかどうか
+        next_page = response.css('div.p-pagination_next > a.button')
+        print(type(next_page))
+        if next_page is not []:
+            print("#####next page#####")
+            next_page_url = response.urljoin(next_page[0].css('a::attr(href)').extract_first())
+            print(next_page_url)
+            yield scrapy.Request(next_page_url, callback=self.parse, errback=self.error_parse)
+        
+            
+            
+    """
     def parse(self, response):
-        """Summary Lines
+       Summary Lines
         本スクレイピング処理。店舗のページにアクセスして情報をitemsに格納する。
         Args:
             response (scrapy.Request): scrapy.Requestで返されたresponseオブジェクト
-        """
+       
+        item = EkitenscrapyItem()
+        print("#####parse#####")
+    """
