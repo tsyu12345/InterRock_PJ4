59a51a656249bd997c8aebb385031f58cb00bd4e
syu_asus_Note <syukunt@icloud.com>
2021-12-10 10:56:27 +0900

    スクレイピング用アイテムインスタンスの配置。

--- ekitenScrapy/ekitenScrapy/spiders/ekitenSpider.py
+++ ekitenScrapy/ekitenScrapy/spiders/ekitenSpider.py
@@ -22,7 +22,7 @@ class EkitenspiderSpider(scrapy.Spider):
         Yields:
             str: middlewareで返却された小ジャンルURL
         
-        middleware = SeleniumMiddlewares(['徳島県'], 4)
+        middleware = SeleniumMiddlewares(item['徳島県'], 4)
         result = middleware.run()
         for url in result:
             yield scrapy.Request(url, callback=self.pre_parse, errback=self.error_parse)
@@ -78,7 +78,7 @@ class EkitenspiderSpider(scrapy.Spider):
             
             
     
-    def hon_parse(self, response):
+    def store_parse(self, response):
         """
         Summary Lines
         本スクレイピング処理。店舗のページにアクセスして情報をitemsに格納する。
@@ -87,4 +87,56 @@ class EkitenspiderSpider(scrapy.Spider):
         """
         item = EkitenscrapyItem()
         print("#####parse#####")
-    
+        
+        """
+        scraping items below
+        item['store_big_junle'] =  #大ジャンル
+        item['store_tel'] =  #電話番号
+        item['store_name'] =  #店名
+        item['str_name_kana'] =  #店名カナ
+        item['price_plan'] =  #料金プラン
+        item['pref_code'] =  #都道府県コード
+        item['pref'] =  #都道府県
+        item['city'] =  #市区町村・番地
+        item['full_address'] =  #住所
+        item['store_link'] =  #scrapingする掲載URL
+        item['shop_id'] =  #shopID
+        item['st_home_page1'] =  #店舗ホームページ1
+        item['st_home_page2'] =  #店舗ホームページ2
+        item['st_home_page3'] =  #店舗ホームページ3
+        item['pankuzu'] =  #パンクズヘッダー
+        item['catch_cp'] =  #キャッチコピー
+        item['is_official'] =  #公式店かどうか
+        item['evaluation_score'] =  #評価点
+        item['review_count'] =  #レビュー件数
+        item['image_count'] =  #画像件数
+        item['access_info'] =  #アクセス情報
+        item['junle1'] =  #ジャンル1
+        item['junle2'] =  #ジャンル2
+        item['junle3'] =  #ジャンル3
+        item['junle4'] =  #ジャンル4
+        item['can_early_morning'] =  #早朝OK
+        item['can_date_celebration'] =  #日祝OK
+        item['can_night'] =  #夜間OK
+        item['has_park'] =  #駐車場あり
+        item['can_rev_net'] =  #ネット予約OK
+        item['has_coupon'] =  #クーポンあり
+        item['can_card'] =  #カード利用OK
+        item['can_delivery'] =  #配達OK
+        item['latitude'] =  #緯度
+        item['longitude'] =  #経度
+        item['closest_station'] =  #最寄り駅
+        item['business_hours'] =  #営業時間
+        item['park_info'] =  #駐車場情報
+        item['credit_info'] =  #クレジットカード情報
+        item['seat_info'] =  #座席情報
+        item['use_case'] =  #用途
+        item['menu'] =  #メニュー
+        item['feature'] =  #特徴
+        item['point'] =  #ポイント
+        item['here_is_great'] =  #ここがすごい
+        item['media_related'] =  #メディア関連
+        item['pricing'] =  #価格設定
+        item['multi_acccess'] =  #マルチアクセス
+        item['introduce'] =  #紹介文
+    """
